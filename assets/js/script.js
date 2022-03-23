f978608e1f08dfd9c211d88a2deb5417d49c2601
unknown <travislnelson92@gmail.com>
2022-03-22 17:06:11 -0700

    add displayForecast() to retrieve and print data from dayjs onto font-awesome styled cards in the ui

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#weather-container");
 var cityTitleEl = document.querySelector("#current-weather-title");
+var forecastTitle = document.querySelector("#forecast-title");
 
 var getCityWeather = function (city) {
-    // openweather api url
+    // openweather api url w/ query string parameters
     var apiUrl ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial";
+
     // make a request to url
     fetch(apiUrl).then(function (response) {
         // request was successful
@@ -18,11 +20,13 @@ var getCityWeather = function (city) {
         }
     })
     .catch(function (error) {
-    alert("Unable to connect to OpenWeather");
+
+        alert("Unable to connect to OpenWeather");
     });
 };
 
 var formSubmitHandler = function (event) {
+    // prevent html refresh
     event.preventDefault();
   
     // get value from input element
@@ -38,39 +42,77 @@ var formSubmitHandler = function (event) {
 };
 
 var displayWeather = function (weatherData) {
-      // format and display data
-  $("#current-weather").addClass("border border-secondary border-2");
-  $("#current-weather-title").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ")
-  .append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    // format and display data
+    $("#current-weather").addClass("border border-info border-5");
+    $("#current-weather-title").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ")
+    .append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
 
     // current temperature
-  $("#current-weather-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+    $("#current-weather-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
     // current  humidity
-  $("#current-weather-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#current-weather-humidity").text("Humidity: " + weatherData.main.humidity + "%");
     // current  wind speed
-  $("#current-weather-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+    $("#current-weather-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
 
-    // lat&lon to get uvi and 5day forecast
-  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + 
-  weatherData.coord.lat + "&lon=" + 
-  weatherData.coord.lon + "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial")
-  .then(function (response) {
-    response.json().then(function (data) {
-      // display the uv index value
-      $("#current-weather-uvi").text("UVI Index: " + data.current.uvi);
+    // fetch for uvi and 5day forecast
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + weatherData.coord.lat + "&lon=" + weatherData.coord.lon + "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial")
 
-        // display 5-day forecast
-        displayForecast(data);
-    });
+    .then(function (response) {
 
-  });
+        response.json().then(function (data) {
+            // display the uv index value
+            $("#current-weather-uvi").text("UVI Index: " + data.current.uvi);
 
+            // display 5-day forecast
+            displayForecast(data);
+        });
+
+     });
+
+
+};
+
+var displayForecast = function (data) {
+    $("#forecast-title").text("5-Day Forecast:");
+
+    // clear previous entries in forecast
+    $("#forecast").empty();
+      
+    // for loop to get data for 5 days
+    for (i = 1; i <= 5; i++) {
+        // create elements div elements for daily weather card
+        var cardDiv = $("<div>").addClass("col-md-2 m-2 py-3 card text-white bg-success");
+        var cardBodyDiv = $("<div>").addClass("card-body p-1");
+
+        // apend card body div to parent card div
+        cardDiv.append(cardBodyDiv);
+
+        // create elements for daily weather card content
+        var cardTitle = $("<h5>").addClass("card-title").text(dayjs(data.daily[i].dt * 1000).format("MM/DD/YYYY"));
+        let imgSrc ="https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+        var cardImg = $("<img>").attr("src", imgSrc).attr("alt", "weather-icon");
+
+        var cardTemp = $("<p>")
+        .addClass("card-text")
+        .text("Temp: " + data.daily[i].temp.day.toFixed(1) + " °F");
+  
+        var cardWind = $("<p>")
+        .addClass("card-text")
+        .text("Wind: " + data.daily[i].wind_speed + " MPH");
+  
+        var cardHumidity = $("<p>")
+        .addClass("card-text")
+        .text("Humidity: " + data.daily[i].humidity + "%");
+
+        // append content elements to parent div
+        cardBodyDiv.append(cardTitle, cardImg, cardTemp, cardWind, cardHumidity);
+
+        //append cards to parent forecast div on the page
+        $("#forecast").append(cardDiv);
+
+    }
 
 };
 
 // event handlers
 $("#search").on("click", formSubmitHandler);
-
-var displayForecast = function (data) {
-    
-}
