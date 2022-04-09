558ef75ddc8505964eac3615fe51dcaf5c7c3415
ValiantThor92 <travislnelson92@gmail.com>
2022-04-08 18:04:42 -0700

    update comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,116 +45,116 @@ var formSubmitHandler = function (event) {
 };
 
 var displayWeather = function (weatherData) {
-    // format and display data
-    $("#current-weather").addClass("border border-info border-5");
-    $("#current-weather-title").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ")
-    .append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+  // format and display data
+  $("#current-weather").addClass("border border-info border-5");
+  $("#current-weather-title").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ")
+  .append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
 
-    // current temperature
-    $("#current-weather-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
-    // current  humidity
-    $("#current-weather-humidity").text("Humidity: " + weatherData.main.humidity + "%");
-    // current  wind speed
-    $("#current-weather-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+  // current temperature
+  $("#current-weather-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+  // current  humidity
+  $("#current-weather-humidity").text("Humidity: " + weatherData.main.humidity + "%");
+  // current  wind speed
+  $("#current-weather-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
 
-    // fetch for uvi and 5day forecast
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + weatherData.coord.lat + "&lon=" + weatherData.coord.lon + "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial")
+  // fetch for uvi and 5day forecast using lat & lon data
+  fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + weatherData.coord.lat + "&lon=" + weatherData.coord.lon + "&appid=a42b1bffc45c35fcb28a1fcc1fc29685&units=imperial")
 
-    .then(function (response) {
+  .then(function (response) {
 
-        response.json().then(function (data) {
-            // display the uv index value
-            $("#current-weather-uvi").text("UVI Index: " + data.current.uvi);
+    response.json().then(function (data) {
+      // display the uv index value
+      $("#current-weather-uvi").text("UVI Index: " + data.current.uvi);
 
-            // display 5-day forecast
-            displayForecast(data);
-        });
+      // display 5-day forecast
+      displayForecast(data);
+    });
 
-     });
+  });
 
-    // save the city
-    searchedCity = weatherData.name;
-    saveSearchedCities(weatherData.name);
+  // save the city
+  searchedCity = weatherData.name;
+  saveSearchedCities(weatherData.name);
 };
 
 var displayForecast = function (data) {
-    $("#forecast-title").text("5-Day Forecast:");
+  $("#forecast-title").text("5-Day Forecast:");
 
-    // clear previous entries in forecast
-    $("#forecast").empty();
-      
-    // for loop to get data for 5 days
-    for (i = 1; i <= 5; i++) {
-        // create elements div elements for daily weather card
-        var cardDiv = $("<div>").addClass("col-md-2 m-2 py-3 card text-white bg-success");
-        var cardBodyDiv = $("<div>").addClass("card-body p-1");
+  // clear previous entries in forecast
+  $("#forecast").empty();
+    
+  // for loop to get data for 5 days
+  for (i = 1; i <= 5; i++) {
+    // create elements div elements for daily weather card
+    var cardDiv = $("<div>").addClass("col-md-2 m-2 py-3 card text-white bg-success");
+    var cardBodyDiv = $("<div>").addClass("card-body p-1");
 
-        // apend card body div to parent card div
-        cardDiv.append(cardBodyDiv);
+    // apend card body div to parent card div
+    cardDiv.append(cardBodyDiv);
 
-        // create elements for daily weather card content
-        var cardTitle = $("<h5>").addClass("card-title").text(dayjs(data.daily[i].dt * 1000).format("MM/DD/YYYY"));
-        let imgSrc ="https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
-        var cardImg = $("<img>").attr("src", imgSrc).attr("alt", "weather-icon");
+    // create elements for daily weather card content
+    var cardTitle = $("<h5>").addClass("card-title").text(dayjs(data.daily[i].dt * 1000).format("MM/DD/YYYY"));
+    let imgSrc ="https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png";
+    var cardImg = $("<img>").attr("src", imgSrc).attr("alt", "weather-icon");
 
-        var cardTemp = $("<p>")
-        .addClass("card-text")
-        .text("Temp: " + data.daily[i].temp.day.toFixed(1) + " °F");
-  
-        var cardWind = $("<p>")
-        .addClass("card-text")
-        .text("Wind: " + data.daily[i].wind_speed + " MPH");
-  
-        var cardHumidity = $("<p>")
-        .addClass("card-text")
-        .text("Humidity: " + data.daily[i].humidity + "%");
+    var cardTemp = $("<p>")
+    .addClass("card-text")
+    .text("Temp: " + data.daily[i].temp.day.toFixed(1) + " °F");
 
-        // append content elements to parent div
-        cardBodyDiv.append(cardTitle, cardImg, cardTemp, cardWind, cardHumidity);
+    var cardWind = $("<p>")
+    .addClass("card-text")
+    .text("Wind: " + data.daily[i].wind_speed + " MPH");
 
-        //append cards to parent forecast div on the page
-        $("#forecast").append(cardDiv);
+    var cardHumidity = $("<p>")
+    .addClass("card-text")
+    .text("Humidity: " + data.daily[i].humidity + "%");
 
-    }
+    // append content elements to parent div
+    cardBodyDiv.append(cardTitle, cardImg, cardTemp, cardWind, cardHumidity);
+
+    //append cards to parent forecast div on the page
+    $("#forecast").append(cardDiv);
+
+  }
 
 };
 
 // save search history to local storage
 var saveSearchedCities = function(city) {
-    if (!searchedCities.includes(city)) {
-        searchedCities.push(city);
-    }
-    localStorage.setItem("searchHistory", JSON.stringify(searchedCities));
-    localStorage.setItem("searchedCity", JSON.stringify(searchedCity));
+  if (!searchedCities.includes(city)) {
+      searchedCities.push(city);
+  }
+  localStorage.setItem("searchHistory", JSON.stringify(searchedCities));
+  localStorage.setItem("searchedCity", JSON.stringify(searchedCity));
 
-    // display searched cities
-    loadSearchedCities();
+  // display searched cities
+  loadSearchedCities();
 }
 
 var loadSearchedCities = function () {
-    searchedCities = JSON.parse(localStorage.getItem("searchHistory"));
-    searchedCity = JSON.parse(localStorage.getItem("searchedCity"));
+  searchedCities = JSON.parse(localStorage.getItem("searchHistory"));
+  searchedCity = JSON.parse(localStorage.getItem("searchedCity"));
 
-    // create empty array and string if nothing saved in local storage
-    if (!searchedCities) {
-        searchedCities = [];
-    }
-    if (!searchedCity) {
-        searchedCity = "";
-    }
+  // create empty array and string if nothing saved in local storage
+  if (!searchedCities) {
+      searchedCities = [];
+  }
+  if (!searchedCity) {
+      searchedCity = "";
+  }
 
-    // clear previous saved cities
-    $("#saved-cities").empty();
+  // clear previous saved cities
+  $("#saved-cities").empty();
 
-    // loop through each city in searchedCities array
-    for (i = 0; i < searchedCities.length; i++) {
+  // loop through each city in searchedCities array
+  for (i = 0; i < searchedCities.length; i++) {
 
-        // create button
-        var cityBtn = $("<button>").addClass("btn btn-secondary").attr("type", "submit").attr("id", searchedCities[i]).text(searchedCities[i]);
+      // create button
+      var cityBtn = $("<button>").addClass("btn btn-secondary").attr("type", "submit").attr("id", searchedCities[i]).text(searchedCities[i]);
 
-        // append button to parent div on the page
-        $("#saved-cities").append(cityBtn);
-    }
+      // append button to parent div on the page
+      $("#saved-cities").append(cityBtn);
+  }
 
 };
 
